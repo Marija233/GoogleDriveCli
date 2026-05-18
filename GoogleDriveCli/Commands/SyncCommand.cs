f867336d9f19b7fdbea2d53261eafa4570f68f13
Marija Cholakova <marijacholakova24@gmail.com>
2026-05-18 03:49:32 +0200

    Add project files.

---
+++ GoogleDriveCli/Commands/SyncCommand.cs
@@ -0,0 +1,17 @@
+﻿using GoogleDriveCli.Services;
+
+namespace GoogleDriveCli.Commands;
+
+public class SyncCommand : ICommand
+{
+    private readonly GoogleDriveService _service;
+    public SyncCommand(GoogleDriveService service)
+    {
+        _service = service;
+    }
+
+    public async Task Execute(string[] args)
+    {
+        await _service.SyncFilesAsync();
+    }
+}
---
+++ GoogleDriveCli/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace GoogleDriveCli.Services;
+
+public class AuthService
+{
+    public async Task<DriveService> AuthenticateAsync()
+    {
+        using var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read);
+
+        var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.FromStream(stream).Secrets,
+            new[] { DriveService.Scope.Drive },
+            "user",
+            CancellationToken.None,
+            new FileDataStore("token_store", true)
+        );
+
+        return new DriveService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "GoogleDriveCli"
+        });
+    }
+}
